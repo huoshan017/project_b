7b7017fc1663d6826209a904f8af58a1cb357295
huoshan017 <huoshan017@hotmail.com>
2023-06-25 14:28:31 +0800

    update

---
+++ common/object/component.go
@@ -0,0 +1,63 @@
+package object
+
+type IComponent interface {
+	Name() string
+}
+
+type AABB struct {
+	Left, Bottom int32
+	Right, Top   int32
+}
+
+// 是否相交
+func (ab *AABB) Intersect(aabb *AABB) bool {
+	return !(ab.Left >= aabb.Right || ab.Right <= aabb.Left || ab.Top <= aabb.Bottom || ab.Bottom >= aabb.Top)
+}
+
+// 移動
+func (ab *AABB) Move(dir Direction, distance float64) {
+	switch dir {
+	case DirLeft:
+		ab.Left -= int32(distance)
+		ab.Right -= int32(distance)
+	case DirRight:
+		ab.Left += int32(distance)
+		ab.Right += int32(distance)
+	case DirUp:
+		ab.Top += int32(distance)
+		ab.Bottom += int32(distance)
+	case DirDown:
+		ab.Top -= int32(distance)
+		ab.Bottom -= int32(distance)
+	}
+}
+
+// 碰撞組件
+type CollisionComp struct {
+	onCollision func(...any)
+}
+
+// 組件名稱
+func (c CollisionComp) Name() string {
+	return "Collision"
+}
+
+// 獲得AABB
+func (c CollisionComp) GetAABB(obj IObject) AABB {
+	return AABB{
+		Left:   obj.Left(),
+		Bottom: obj.Bottom(),
+		Right:  obj.Right(),
+		Top:    obj.Top(),
+	}
+}
+
+// 設置碰撞事件處理
+func (c *CollisionComp) SetOnCollisionEventHandle(onCollision func(...any)) {
+	c.onCollision = onCollision
+}
+
+// 獲得碰撞事件處理函數
+func (c CollisionComp) GetOnCollisionEventHandle() func(...any) {
+	return c.onCollision
+}
